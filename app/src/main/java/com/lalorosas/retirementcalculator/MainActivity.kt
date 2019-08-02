d7c82026ca94bfd61c7577e19520a72744655b9c
Eduardo Rosas <lalorosas@outlook.com>
2019-08-02 16:46:57 -0500

    Initial commit

---
+++ app/src/main/java/com/lalorosas/retirementcalculator/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.lalorosas.retirementcalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        AppCenter.start(application, "6161a2f1-302f-4a81-b78f-37c0eb4f22f7", Analytics::class.java, Crashes::class.java);
+    }
+}
