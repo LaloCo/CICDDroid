1b1bf45232434e64353c96c2d64c8b34c44bd0cf
Eduardo Rosas <lalorosas@outlook.com>
2019-08-06 10:15:29 -0500

    Crashing the application

--- app/src/main/java/com/lalorosas/retirementcalculator/MainActivity.kt
+++ app/src/main/java/com/lalorosas/retirementcalculator/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import com.microsoft.appcenter.AppCenter
 import com.microsoft.appcenter.analytics.Analytics
 import com.microsoft.appcenter.crashes.Crashes
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,5 +14,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         AppCenter.start(application, "6161a2f1-302f-4a81-b78f-37c0eb4f22f7", Analytics::class.java, Crashes::class.java);
+
+        calculateButton.setOnClickListener {
+            Crashes.generateTestCrash()
+        }
     }
 }
