440274b4b64ad701acb4f2d79470d40f52150934
Eduardo Rosas <lalorosas@outlook.com>
2019-12-16 22:48:05 -0600

    This will work

--- app/appcenter-pre-build.sh
+++ app/appcenter-pre-build.sh
@@ -9,7 +9,7 @@ ANDROID_GRADLE_FILE=$APPCENTER_SOURCE_DIRECTORY/app/build.gradle
 if [ -e "$ANDROID_GRADLE_FILE" ]
 then
     echo "Updating version name to $VERSION_NAME ($APPCENTER_BUILD_ID) in build.gradle"
-    sed -i 's/versionName="[0-9.]*"/versionName="'$VERSION_NAME'"/' $ANDROID_GRADLE_FILE
+    sed -i 's/versionName "[0-9.]*"/versionName "'$VERSION_NAME'"/' $ANDROID_GRADLE_FILE
 
     echo "File content:"
     cat $ANDROID_GRADLE_FILE
